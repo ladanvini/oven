ce4db694d9519b1a5da68da8cd06d0181ff55e5d
ladanvini <ladan.pourvatan@gmail.com>
2023-04-07 14:46:49 +0200

    add missing annotations

--- oven/oven/src/main/java/com/vini/oven/services/OvenService.java
+++ oven/oven/src/main/java/com/vini/oven/services/OvenService.java
@@ -1,7 +1,10 @@
 package com.vini.oven.services;
 
+import org.springframework.stereotype.Service;
+
 import com.vini.oven.entities.Oven;
 
+@Service
 public class OvenService {
 	private Oven oven;
 	public OvenService() {
