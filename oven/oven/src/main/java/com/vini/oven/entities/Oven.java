945cabc7a2f8a543152fb0e30aecb60581e74a6d
ladanvini <ladan.pourvatan@gmail.com>
2023-04-11 20:56:31 +0200

    set up application to use postgres

--- oven/oven/src/main/java/com/vini/oven/entities/Oven.java
+++ oven/oven/src/main/java/com/vini/oven/entities/Oven.java
@@ -1,12 +1,34 @@
 package com.vini.oven.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "oven")
 public class Oven {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "light")
 	private Boolean light;
+
+	@Column(name = "upper_element")
 	private int upper_element;
+
+	@Column(name = "lower_element")
 	private int lower_element;
+
+	@Column(name = "grill_temp")
 	private int grill_temp;
+
+	@Column(name = "fan_speed")
 	private int fan_speed;
-	
+
 	public Oven() {
 		this.setLight(false);
 		this.setUpper_element(0);
@@ -14,14 +36,16 @@ public class Oven {
 		this.setGrill_temp(0);
 		this.setFan_speed(0);
 	}
+
 	public Oven(Boolean _light, int _upper, int _lower, int _grill, int _fan_speed) {
 		this.setFan_speed(_fan_speed);
 		this.setGrill_temp(_grill);
 		this.setLight(_light);
 		this.setLower_element(_lower);
 		this.setUpper_element(_upper);
-		
+
 	}
+
 	public int getFan_speed() {
 		return fan_speed;
 	}
@@ -61,7 +85,7 @@ public class Oven {
 	public void setLight(Boolean light) {
 		this.light = light;
 	}
-	
+
 	public String toString() {
 		String light_str = "Lights: ";
 		if (this.light)
@@ -71,9 +95,9 @@ public class Oven {
 		String upper_str = "Upper Element Temp: " + Integer.toString(this.upper_element);
 		String lower_str = "Lower Element Temp:" + Integer.toString(this.lower_element);
 		String grill_str = "Grill Temp: " + Integer.toString(this.grill_temp);
-		String fan_str = "Fan Speed: " + Integer.toString(this.fan_speed); 
-		
-		return ("\n" + light_str + "\n" + upper_str  + "\n" + lower_str  + "\n" + grill_str  + "\n" + fan_str + "\n");
+		String fan_str = "Fan Speed: " + Integer.toString(this.fan_speed);
+
+		return ("\n" + light_str + "\n" + upper_str + "\n" + lower_str + "\n" + grill_str + "\n" + fan_str + "\n");
 	}
-	
+
 }
--- oven/oven/src/main/java/com/vini/oven/repositories/OvenRepository.java
+++ oven/oven/src/main/java/com/vini/oven/repositories/OvenRepository.java
@@ -1,56 +1,13 @@
 package com.vini.oven.repositories;
+import java.util.List;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.vini.oven.entities.Oven;
 
 @Repository
-public class OvenRepository {
-	private Oven oven;
-	private String path_to_static_resources = "./src/main/resources/static/"; 
-	public OvenRepository() {
-	}
-	private JSONObject read_object_from_storage() throws Exception {
-		JSONParser jsonParser = new JSONParser();
-		try (FileReader reader = new FileReader(path_to_static_resources+"oven_state.json")) {
-			// Read JSON file
-			Object obj = jsonParser.parse(reader);
-			JSONObject oven_obj = (JSONObject) obj;
-			
-			return oven_obj;
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new Exception("My Internal Exception to do");
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new Exception("My Internal Exception to do");
+public interface OvenRepository extends CrudRepository<Oven, Integer>{
+	List<Oven> findAll();
 
-		} catch (ParseException e) {
-			e.printStackTrace();
-			throw new Exception("My Internal Exception to do");
-
-		}
-
-	}
-	private void update_states_from_json(JSONObject oven_obj) {
-		Boolean light = (Boolean)oven_obj.get("light");
-		int upper = ((Long)oven_obj.get("upper_element")).intValue() ;
-		int lower = ((Long)oven_obj.get("lower_element")).intValue()  ;
-		int grill = ((Long)oven_obj.get("grill_temp")).intValue()  ;
-		int fan = ((Long)oven_obj.get("fan_speed")).intValue()  ;
-		oven = new Oven(light, upper, lower, grill, fan);
-	}
-	public Oven get_oven_state() throws Exception {
-		JSONObject json_obj = this.read_object_from_storage();
-		this.update_states_from_json(json_obj);
-		return this.oven;
-	}
 }
