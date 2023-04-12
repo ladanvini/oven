ca44e6141b3685479c7def8d53bbbe32c7000030
ladanvini <ladan.pourvatan@gmail.com>
2023-04-12 18:52:47 +0200

    cleanup oven controller

--- oven/oven/src/main/java/com/vini/oven/controllers/OvenController.java
+++ oven/oven/src/main/java/com/vini/oven/controllers/OvenController.java
@@ -16,13 +16,22 @@ public class OvenController {
 
 	@GetMapping("/")
 	public String home() {
-		ovenService.save_dummy_oven();
+		String greeting = "Welcome to Oven Service!\r\n\r\n";
+		greeting += "I should be running on your localhost:8080\n";
+		String curl_with_paths = "Go ahead and curl me with any of the following endpoints:\n";
+		curl_with_paths += "/all";
+		return greeting + curl_with_paths;
+	}
+
+	@GetMapping("/all")
+	public String all_ovens() {
 		List<Oven> all_ovens = this.ovenService.show_all_ovens();
-		
+
 		String stingified_ovens = "\n";
-		for (int i=0; i < all_ovens.size(); i++) {
+		for (int i = 0; i < all_ovens.size(); i++) {
 			stingified_ovens += all_ovens.get(i).toString();
 		}
 		return stingified_ovens;
 	}
+
 }
