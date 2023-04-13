3bc407a6e497e78041a03f35713267c8f0ded1be
ladanvini <ladan.pourvatan@gmail.com>
2023-04-13 13:20:31 +0200

    feat: OvenService shows all ovens

--- oven/oven/src/main/java/com/vini/oven/services/OvenService.java
+++ oven/oven/src/main/java/com/vini/oven/services/OvenService.java
@@ -1,5 +1,6 @@
 package com.vini.oven.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +12,20 @@ import com.vini.oven.repositories.OvenRepository;
 @Service
 public class OvenService {
 	@Autowired
-	private OvenRepository ovenRepo;
+	private OvenRepository oven_repository;
 
-	public List<Oven> show_all_ovens() {
-		return ovenRepo.findAll();
+	public List<String> showAllOvensStr() {
+		List<Oven> all_ovens = oven_repository.findAll();
+		List<String> all_ovens_str = new ArrayList<String>();
+		for(int i=0; i<all_ovens.size(); i++) {
+			all_ovens_str.add(all_ovens.get(i).toString());
+		}
+		return all_ovens_str;
 
 	}
 
 	public void save_dummy_oven() {
-		ovenRepo.save(new Oven());
-		ovenRepo.save(new Oven(true, 10, 10, 0, 100));
+		oven_repository.save(new Oven());
+		oven_repository.save(new Oven(true, 10, 10, 0, 100));
 	}
 }
