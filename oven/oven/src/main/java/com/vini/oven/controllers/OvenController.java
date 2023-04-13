a21dd9475527b3e22e3658e20440f1580a4ae7e7
ladanvini <ladan.pourvatan@gmail.com>
2023-04-13 17:09:26 +0200

    feat: get all ovens controller

--- oven/oven/src/main/java/com/vini/oven/controllers/OvenController.java
+++ oven/oven/src/main/java/com/vini/oven/controllers/OvenController.java
@@ -10,27 +10,30 @@ import com.vini.oven.services.OvenService;
 
 @RestController
 public class OvenController {
-	@Autowired
-	private OvenService ovenService;
+    @Autowired
+    private OvenService ovenService;
 
-	@GetMapping("/")
-	public String home() {
-		String greeting = "Welcome to Oven Service!\r\n\r\n";
-		greeting += "I should be running on your localhost:8080\n";
-		String curl_with_paths = "Go ahead and curl me with any of the following endpoints:\n";
-		curl_with_paths += "/all";
-		return greeting + curl_with_paths;
-	}
+    @GetMapping("/")
+    public String home() {
+	String greeting = "Welcome to Oven Service!\r\n\r\n";
+	greeting += "I should be running on your localhost:8080\n";
+	String curl_with_paths = "Go ahead and curl me with any of the following endpoints:\n";
+	curl_with_paths += "/all";
+	return greeting + curl_with_paths;
+    }
 
-	@GetMapping("/all")
-	public String all_ovens() {
-		List<String> all_ovens = ovenService.showAllOvensStr();
-		String title = "Here are all of my ovens!";
-		String stingified_ovens = "\n";
-		for (int i = 0; i < all_ovens.size(); i++) {
-			stingified_ovens += (all_ovens.get(i) + "\n");
-		}
-		return title + stingified_ovens;
+    @GetMapping("/all")
+    public String allOvens() {
+	List<String> all_ovens = ovenService.showAllOvensStr();
+	if (all_ovens == null || all_ovens.size() == 0) {
+	    return "\nOops! I have no ovens!\n";
 	}
+	String title = "Here are all of my ovens!";
+	String stingified_ovens = "\n";
+	for (int i = 0; i < all_ovens.size(); i++) {
+	    stingified_ovens += (all_ovens.get(i) + "\n");
+	}
+	return title + stingified_ovens;
+    }
 
 }
