0b7dcfe8934eddf001bf6a16a30983b1b940a08c
ladanvini <ladan.pourvatan@gmail.com>
2023-04-13 20:41:30 +0200

    fix: order all by id asc

--- oven/oven/src/main/java/com/vini/oven/services/OvenService.java
+++ oven/oven/src/main/java/com/vini/oven/services/OvenService.java
@@ -11,21 +11,21 @@ import com.vini.oven.repositories.OvenRepository;
 
 @Service
 public class OvenService {
-	@Autowired
-	private OvenRepository oven_repository;
+    @Autowired
+    private OvenRepository oven_repository;
 
-	public List<String> showAllOvensStr() {
-		List<Oven> all_ovens = oven_repository.findAll();
-		List<String> all_ovens_str = new ArrayList<String>();
-		for(int i=0; i<all_ovens.size(); i++) {
-			all_ovens_str.add(all_ovens.get(i).toString());
-		}
-		return all_ovens_str;
-
+    public List<String> showAllOvensStr() {
+	List<Oven> all_ovens = oven_repository.findAllByOrderByIdAsc();
+	List<String> all_ovens_str = new ArrayList<String>();
+	for (int i = 0; i < all_ovens.size(); i++) {
+	    all_ovens_str.add(all_ovens.get(i).toString());
 	}
+	return all_ovens_str;
 
-	public void save_dummy_oven() {
-		oven_repository.save(new Oven());
-		oven_repository.save(new Oven(true, 10, 10, 0, 100));
-	}
+    }
+
+    public void save_dummy_oven() {
+	oven_repository.save(new Oven());
+	oven_repository.save(new Oven(true, 10, 10, 0, 100));
+    }
 }
--- oven/oven/src/test/java/com/vini/oven/services/unit/ShowAllOvensStrTest.java
+++ oven/oven/src/test/java/com/vini/oven/services/unit/ShowAllOvensStrTest.java
@@ -20,55 +20,55 @@ import com.vini.oven.services.OvenService;
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class ShowAllOvensStrTest {
-	@Autowired
-	private OvenService oven_service;
+    @Autowired
+    private OvenService oven_service;
 
-	@MockBean
-	private OvenRepository oven_repository;
+    @MockBean
+    private OvenRepository oven_repository;
 
-	@Test
-	public void testReturnsOneCustomOven() throws Exception {
-		Oven test_oven = new Oven(true, 0, 10, 0, 100);
-		List<Oven> repo_oven_list = new ArrayList<Oven>();
-		repo_oven_list.add(test_oven);
-		List<String> expected = new ArrayList<String>();
-		expected.add(test_oven.toString());
+    @Test
+    public void testReturnsOneCustomOven() throws Exception {
+	Oven test_oven = new Oven(true, 0, 10, 0, 100);
+	List<Oven> repo_oven_list = new ArrayList<Oven>();
+	repo_oven_list.add(test_oven);
+	List<String> expected = new ArrayList<String>();
+	expected.add(test_oven.toString());
 
-		when(oven_repository.findAll()).thenReturn(repo_oven_list);
+	when(oven_repository.findAllByOrderByIdAsc()).thenReturn(repo_oven_list);
 
-		List<String> actual = oven_service.showAllOvensStr();
-		assertEquals(expected, actual);
-	}
+	List<String> actual = oven_service.showAllOvensStr();
+	assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testReturnsEmptyListWhenNoOvens() throws Exception {
-		List<String> expected = new ArrayList<String>();
+    @Test
+    public void testReturnsEmptyListWhenNoOvens() throws Exception {
+	List<String> expected = new ArrayList<String>();
 
-		when(oven_repository.findAll()).thenReturn(new ArrayList<Oven>());
+	when(oven_repository.findAllByOrderByIdAsc()).thenReturn(new ArrayList<Oven>());
 
-		List<String> actual = oven_service.showAllOvensStr();
-		assertEquals(expected, actual);
-	}
+	List<String> actual = oven_service.showAllOvensStr();
+	assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testReturnsMultipleOvens() throws Exception {
-		Oven test_oven_0 = new Oven(true, 0, 10, 0, 100);
-		Oven test_oven_1 = new Oven(false, 32, 0, 1, 0);
-		Oven test_oven_2 = new Oven();
-		List<Oven> repo_oven_list = new ArrayList<Oven>();
-		repo_oven_list.add(test_oven_0);
-		repo_oven_list.add(test_oven_1);
-		repo_oven_list.add(test_oven_2);
+    @Test
+    public void testReturnsMultipleOvens() throws Exception {
+	Oven test_oven_0 = new Oven(true, 0, 10, 0, 100);
+	Oven test_oven_1 = new Oven(false, 32, 0, 1, 0);
+	Oven test_oven_2 = new Oven();
+	List<Oven> repo_oven_list = new ArrayList<Oven>();
+	repo_oven_list.add(test_oven_0);
+	repo_oven_list.add(test_oven_1);
+	repo_oven_list.add(test_oven_2);
 
-		List<String> expected = new ArrayList<String>();
-		expected.add(test_oven_0.toString());
-		expected.add(test_oven_1.toString());
-		expected.add(test_oven_2.toString());
+	List<String> expected = new ArrayList<String>();
+	expected.add(test_oven_0.toString());
+	expected.add(test_oven_1.toString());
+	expected.add(test_oven_2.toString());
 
-		when(oven_repository.findAll()).thenReturn(repo_oven_list);
+	when(oven_repository.findAllByOrderByIdAsc()).thenReturn(repo_oven_list);
 
-		List<String> actual = oven_service.showAllOvensStr();
-		assertEquals(expected, actual);
-	}
+	List<String> actual = oven_service.showAllOvensStr();
+	assertEquals(expected, actual);
+    }
 
 }
