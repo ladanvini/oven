07b092a599c98fa7aa335b99098f569847052dcc
ladanvini <ladan.pourvatan@gmail.com>
2023-04-13 14:37:14 +0200

    feat: oven controller greeting

--- oven/oven/src/test/java/com/vini/oven/controllers/unit/HomeTest.java
+++ oven/oven/src/test/java/com/vini/oven/controllers/unit/HomeTest.java
@@ -1,6 +1,5 @@
-package com.vini.oven.controllers;
+package com.vini.oven.controllers.unit;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,24 +11,30 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.vini.oven.controllers.OvenController;
 import com.vini.oven.services.OvenService;
 
 @WebMvcTest(OvenController.class)
-public class OvenControllerUnitTest {
+public class HomeTest {
+
     @Autowired
     private MockMvc mvc;
-    
     @MockBean
-    private OvenService ovenService;
+    private OvenService oven_service;
 
     @Test
-    public void testWithMockedService() throws Exception {
-		
-		String expected = "Hello world";
-		when(ovenService.show_my_oven_status()).thenReturn(expected);
+    public void testShowsGreetings() {
 
-    	this.mvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-		.andExpect(content().string(expected));
+	String expected = "Welcome to Oven Service!\r\n\r\n"
+		+ "I should be running on your localhost:8080\n"
+		+ "Go ahead and curl me with any of the following endpoints:\n"
+		+ "/all";
+
+	try {
+	    mvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(expected));
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
     }
 
 }
