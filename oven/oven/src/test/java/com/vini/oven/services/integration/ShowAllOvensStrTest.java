2f342c85b7d867b58153a98063d549c119dc8bc9
ladanvini <ladan.pourvatan@gmail.com>
2023-04-14 22:44:10 +0200

    feat: add oven key property

--- oven/oven/src/test/java/com/vini/oven/services/integration/ShowAllOvensStrTest.java
+++ oven/oven/src/test/java/com/vini/oven/services/integration/ShowAllOvensStrTest.java
@@ -19,68 +19,68 @@ import com.vini.oven.services.OvenService;
 @SpringBootTest()
 @ActiveProfiles("test")
 public class ShowAllOvensStrTest {
-	@Autowired
-	private OvenService oven_service;
-	@Autowired
-	private OvenRepository oven_repository;
+    @Autowired
+    private OvenService oven_service;
+    @Autowired
+    private OvenRepository oven_repository;
 
-	@BeforeEach
-	public void setUp() {
-		oven_repository.deleteAll();
-	}
+    @BeforeEach
+    public void setUp() {
+	oven_repository.deleteAll();
+    }
 
-	@Test
-	public void testShowSingleEmptyOvenInCorrectStrFormat() {
-		oven_repository.save(new Oven());
+    @Test
+    public void testShowSingleEmptyOvenInCorrectStrFormat() {
+	oven_repository.save(new Oven("Dumbledore"));
 
-		List<String> actual = oven_service.showAllOvensStr();
+	List<String> actual = oven_service.showAllOvensStr();
 
-		String light_str = "Lights: ";
-		light_str += "OFF";
-		String upper_str = "Upper Element Temp: " + Integer.toString(0);
-		String lower_str = "Lower Element Temp:" + Integer.toString(0);
-		String grill_str = "Grill Temp: " + Integer.toString(0);
-		String fan_str = "Fan Speed: " + Integer.toString(0);
+	String expected_oven_str = "\nOven Key: Dumbledore\n"
+		+ "Lights: OFF\n"
+		+ "Upper Element Temp: 0\n"
+		+ "Lower Element Temp: 0\n"
+		+ "Grill Temp: 0\n"
+		+ "Fan Speed: 0\n";
 
-		List<String> expected = new ArrayList<String>();
-		expected.add("\n" + light_str + "\n" + upper_str + "\n" + lower_str + "\n" + grill_str + "\n" + fan_str + "\n");
+	List<String> expected = new ArrayList<String>();
+	expected.add(expected_oven_str);
 
-		assertEquals(expected, actual);
+	assertEquals(expected, actual);
 
-	}
+    }
 
-	@Test
-	public void testReturnEmptyWhenNoOvensInRepo() {
+    @Test
+    public void testReturnEmptyWhenNoOvensInRepo() {
 
-		List<String> actual = oven_service.showAllOvensStr();
-		List<String> expected = new ArrayList<String>();
+	List<String> actual = oven_service.showAllOvensStr();
+	List<String> expected = new ArrayList<String>();
 
-		assertEquals(expected, actual);
-	}
+	assertEquals(expected, actual);
+    }
 
-	@Test
-	public void testReturnCorrectListOfOvensWhenMultiple() {
-		Oven test_oven_0 = new Oven(true, 0, 10, 0, 100);
-		Oven test_oven_1 = new Oven(false, 32, 0, 1, 0);
-		Oven test_oven_2 = new Oven();
-		List<Oven> ovens_to_save = new ArrayList<Oven>();
-		ovens_to_save.add(test_oven_0);
-		ovens_to_save.add(test_oven_1);
-		ovens_to_save.add(test_oven_2);
-		oven_repository.saveAll(ovens_to_save);
+    @Test
+    public void testReturnCorrectListOfOvensWhenMultiple() {
+	Oven test_oven_0 = new Oven("Fineas", true, 0, 10, 0, 100);
+	Oven test_oven_1 = new Oven("Regulus", false, 32, 0, 1, 0);
+	Oven test_oven_2 = new Oven("Dumbledore");
+	List<Oven> ovens_to_save = new ArrayList<Oven>();
+	ovens_to_save.add(test_oven_0);
+	ovens_to_save.add(test_oven_1);
+	ovens_to_save.add(test_oven_2);
+	oven_repository.saveAll(ovens_to_save);
 
-		List<String> expected = new ArrayList<String>();
-		expected.add(test_oven_0.toString());
-		expected.add(test_oven_1.toString());
-		expected.add(test_oven_2.toString());
+	List<String> expected = new ArrayList<String>();
+	expected.add(test_oven_0.toString());
+	expected.add(test_oven_1.toString());
+	expected.add(test_oven_2.toString());
 
-		List<String> actual = oven_service.showAllOvensStr();
-		assertEquals(expected, actual);
-	}
+	List<String> actual = oven_service.showAllOvensStr();
+	assertEquals(expected, actual);
+    }
 
-	@AfterEach
-	public void tearDown() {
-		oven_repository.deleteAll();
-	}
+    @AfterEach
+    public void tearDown() {
+	oven_repository.deleteAll();
+    }
 
 }
