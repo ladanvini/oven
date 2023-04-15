c99d18e0c58d23df3689b9a54a65cc7046047121
ladanvini <ladan.pourvatan@gmail.com>
2023-04-15 10:22:16 +0200

    test(unit): controller - get oven by key

--- oven/oven/src/test/java/com/vini/oven/controllers/unit/oven_controller_tests/GetOvenByKeyTest.java
+++ oven/oven/src/test/java/com/vini/oven/controllers/unit/oven_controller_tests/GetOvenByKeyTest.java
@@ -1,5 +1,6 @@
 package com.vini.oven.controllers.unit.oven_controller_tests;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -12,6 +13,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.vini.oven.controllers.OvenController;
+import com.vini.oven.entities.Oven;
+import com.vini.oven.exceptions.MyCustomInternalExceptions;
 import com.vini.oven.services.OvenService;
 
 @WebMvcTest(OvenController.class)
@@ -21,13 +24,16 @@ public class GetOvenByKeyTest {
     @MockBean
     private OvenService oven_service;
 
-    private final String path_url = "/ovens/";
+    private final String path_url = "/ovens";
 
     @Test
-    public void testShowsCorrectOven() {
-	String expected = "OVEN";
+    public void testShowsCorrectOven() throws MyCustomInternalExceptions {
+	String oven_key = "Ignatious";
+	Oven test_oven = new Oven(oven_key, true, 10, 10, 0, 100);
+	when(this.oven_service.getOvenByKey(oven_key)).thenReturn(test_oven);
+	String expected = test_oven.toString();
 	try {
-	    mvc.perform(get(this.path_url)).andDo(print()).andExpect(status().isOk())
+	    mvc.perform(get(this.path_url + "/" + oven_key)).andDo(print()).andExpect(status().isOk())
 		    .andExpect(content().string(expected));
 	} catch (Exception e) {
 	    e.printStackTrace();
@@ -35,11 +41,14 @@ public class GetOvenByKeyTest {
     }
 
     @Test
-    public void testShowsNotFoundWhenOvenDoesNotExist() {
+    public void testShowsNotFoundWhenOvenDoesNotExist() throws MyCustomInternalExceptions {
+	String oven_key = "Ignatious";
+	when(this.oven_service.getOvenByKey(oven_key))
+		.thenThrow(new MyCustomInternalExceptions("msg", "oven.not_found"));
 	String expected = "404 Oven Not Found";
 
 	try {
-	    mvc.perform(get(this.path_url)).andDo(print()).andExpect(status().isOk())
+	    mvc.perform(get(this.path_url + "/" + oven_key)).andDo(print()).andExpect(status().isOk())
 		    .andExpect(content().string(expected));
 	} catch (Exception e) {
 	    e.printStackTrace();
@@ -48,11 +57,11 @@ public class GetOvenByKeyTest {
     }
 
     @Test
-    public void testShowsBadRequestWhenInvalidKey() {
-	String expected = "400 Invalid Oven Key";
+    public void testShowsOvenNotFoundWhenOvenNull() {
+	String expected = "404 Oven Not Found";
 
 	try {
-	    mvc.perform(get(this.path_url)).andDo(print()).andExpect(status().isOk())
+	    mvc.perform(get(this.path_url + "/true")).andDo(print()).andExpect(status().isOk())
 		    .andExpect(content().string(expected));
 	} catch (Exception e) {
 	    e.printStackTrace();
@@ -61,11 +70,15 @@ public class GetOvenByKeyTest {
     }
 
     @Test
-    public void testShowsUnavailableIfCantGetStateFromOtherMicroservice() {
+    public void testShowsUnavailableIfCantGetStateFromOtherMicroservice() throws MyCustomInternalExceptions {
+	String oven_key = "Ignatious";
+	when(this.oven_service.getOvenByKey(oven_key))
+		.thenThrow(new MyCustomInternalExceptions("msg", "service.cannot_receive_message"));
+
 	String expected = "503 Service is Currently Unavailable";
 
 	try {
-	    mvc.perform(get(this.path_url)).andDo(print()).andExpect(status().isOk())
+	    mvc.perform(get(this.path_url + "/" + oven_key)).andDo(print()).andExpect(status().isOk())
 		    .andExpect(content().string(expected));
 	} catch (Exception e) {
 	    e.printStackTrace();
