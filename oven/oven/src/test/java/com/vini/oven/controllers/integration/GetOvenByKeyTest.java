8d39eb1bb9f5693837449ff75105d71a497876f0
ladanvini <ladan.pourvatan@gmail.com>
2023-04-16 17:01:39 +0200

    test(integ): GET ovens/{oven_key}

--- oven/oven/src/test/java/com/vini/oven/controllers/integration/GetOvenByKeyTest.java
+++ oven/oven/src/test/java/com/vini/oven/controllers/integration/GetOvenByKeyTest.java
@@ -1,5 +1,6 @@
 package com.vini.oven.controllers.integration;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -11,54 +12,84 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.vini.oven.entities.Oven;
+import com.vini.oven.exceptions.MyCustomInternalExceptions;
 import com.vini.oven.repositories.OvenRepository;
+import com.vini.oven.services.OvenStateCaller;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public class GetOvenByKeyTest {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
     @Autowired
-    private OvenRepository oven_repository;
+    private OvenRepository ovenRepository;
+
+    @MockBean
+    private OvenStateCaller ovenStateCaller;
 
     @BeforeEach
     public void setUp() {
-	oven_repository.deleteAll();
+	ovenRepository.deleteAll();
     }
 
     @AfterEach
     public void tearDown() {
-	oven_repository.deleteAll();
+	ovenRepository.deleteAll();
     }
 
     @Test
-    public void testShowsCorrectOven() throws Exception {
+    public void testShowsCorrectOvenWhenNoChange() throws Exception {
 	Oven expected_oven = new Oven("Hermoine", true, 0, 0, 0, 199);
-	oven_repository.save(expected_oven);
-	oven_repository.save(new Oven("Harry"));
-	oven_repository.save(new Oven("Ron"));
+	ovenRepository.save(expected_oven);
+	ovenRepository.save(new Oven("Harry"));
+	ovenRepository.save(new Oven("Ron"));
+
+	when(ovenStateCaller.getOvenWithUpdatedModeFromOvenStateSvc(expected_oven)).thenReturn(expected_oven);
+
 	String expected = "\nOven Key: Hermoine"
 		+ "\nLights: ON\nUpper Element Temp: 0\nLower Element Temp: 0"
 		+ "\nGrill Temp: 0\nFan Speed: 199\n";
 
-	this.mvc.perform(get("/ovens/Hermoine")).andDo(print()).andExpect(status().isOk())
+	mockMvc.perform(get("/ovens/Hermoine")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(expected));
+    }
+
+    @Test
+    public void testShowsCorrectOvenWhenStateChanged() throws Exception {
+	Oven test_oven = new Oven("Hermoine", true, 0, 0, 0, 199);
+	ovenRepository.save(test_oven);
+	ovenRepository.save(new Oven("Harry"));
+	ovenRepository.save(new Oven("Ron"));
+	Oven expected_oven = new Oven("Hermoine", true, 100, 10, 0, 0);
+	when(ovenStateCaller.getOvenWithUpdatedModeFromOvenStateSvc(test_oven)).thenReturn(expected_oven);
+
+	String expected = "\nOven Key: Hermoine"
+		+ "\nLights: ON\nUpper Element Temp: 100\nLower Element Temp: 10"
+		+ "\nGrill Temp: 0\nFan Speed: 0\n";
+
+	mockMvc.perform(get("/ovens/Hermoine")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(expected));
     }
 
     @Test
     public void testShowsOvenNotFoundWhenNoneWithKeyExist() throws Exception {
 	Oven expected_oven = new Oven("Hermoine", true, 0, 0, 0, 199);
-	oven_repository.save(expected_oven);
-	oven_repository.save(new Oven("Harry"));
-	oven_repository.save(new Oven("Ron"));
+	ovenRepository.save(expected_oven);
+	ovenRepository.save(new Oven("Harry"));
+	ovenRepository.save(new Oven("Ron"));
+
+	when(ovenStateCaller.getOvenWithUpdatedModeFromOvenStateSvc(expected_oven)).thenReturn(expected_oven);
+
 	String expected = "404 Oven Not Found\n";
 
-	this.mvc.perform(get("/ovens/Neville")).andDo(print()).andExpect(status().isOk())
+	mockMvc.perform(get("/ovens/Neville")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(expected));
     }
 
@@ -66,15 +97,21 @@ public class GetOvenByKeyTest {
     public void testShowsOvenNotFoundWhenNoOvensExist() throws Exception {
 	String expected = "404 Oven Not Found\n";
 
-	this.mvc.perform(get("/ovens/Hermoine")).andDo(print()).andExpect(status().isOk())
+	mockMvc.perform(get("/ovens/Hermoine")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(expected));
     }
 
     @Test
-    public void testShowsServiceUnavailable() throws Exception {
+    public void testShowsServiceUnavailableWhenCallerThrows() throws Exception {
+	Oven expected_oven = new Oven("Luna", true, 0, 0, 0, 199);
+	ovenRepository.save(expected_oven);
+
+	when(ovenStateCaller.getOvenWithUpdatedModeFromOvenStateSvc(expected_oven))
+		.thenThrow(new MyCustomInternalExceptions("400 Bad Request", "service.cannot_receive_message"));
+
 	String expected = "503 Service is Currently Unavailable\n";
 
-	this.mvc.perform(get("/ovens/Hermoine")).andDo(print()).andExpect(status().isOk())
+	mockMvc.perform(get("/ovens/Luna")).andDo(print()).andExpect(status().isOk())
 		.andExpect(content().string(expected));
     }
 
